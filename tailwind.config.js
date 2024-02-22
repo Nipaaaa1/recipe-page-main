32ce9646bd36081fa09c7ef495ca9ca6eb7b45d4
Nipaaaa1 <Frostone224@gmail.com>
2024-02-22 19:05:31 +0700

    chore: setup colors and font on tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          nutmeg: "hsl(14, 45%, 36%)",
+          darkRaspberry: "hsl(332, 51%, 32%)",
+        },
+        neutral: {
+          roseWhtie: "hsl(330, 100%, 98%)",
+          eggshel: "hsl(30, 54%, 90%)",
+          lightGrey: "hsl(30, 18%, 87%)",
+          wengeBrown: "hsl(30, 10%, 34%)",
+          darkCharcoal: "hsl(24, 5%, 18%)",
+        },
+      },
+      fontFamily: {
+        youngSerif: ["Young Serif", "serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
